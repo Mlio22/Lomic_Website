d2c5efbee8feeb0c7a46821cb692fc4be6b64d7c
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2018-10-28 10:42:15 +0700

    git add.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,15 @@ $(document).ready(function() {
         if ($('.arrow.fa').hasClass('fa-arrow-right')) {
             $('.fa-arrow-right').removeClass('fa-arrow-right').addClass('fa-arrow-left');
             $('.navbar.navbarAtas').addClass('showed');
+            $('.navbar.navbarKiri.notShowed').removeClass('notShowed');
+            $('.navbar.navbarKiri ul').removeClass('notShowed');
             console.log('berhasil kanan');
         } else {
             $('.fa-arrow-left').removeClass('fa-arrow-left').addClass('fa-arrow-right');
             $('.navbar.navbarAtas.showed').removeClass('showed');
+            $('.navbar.navbarKiri').addClass('notShowed');
+            $('.navbar.navbarKiri ul').addClass('notShowed');
             console.log('berhasil kiri');
         }
     });
-
 });
