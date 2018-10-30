46433848680a4d7fcedba3f5976a01b8647cd227
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2018-10-30 09:06:23 +0700

    git push -u website

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
             $('.fa-arrow-right').removeClass('fa-arrow-right').addClass('fa-arrow-left');
             $('.navbar.navbarAtas').addClass('showed');
             $('.navbar.navbarKiri.notShowed').removeClass('notShowed');
+            $('.isi.notShowed').removeClass('notShowed');
             // $('.navbar.navbarKiri ul').removeClass('notShowed');
             console.log('berhasil kanan');
         } else {
             $('.fa-arrow-left').removeClass('fa-arrow-left').addClass('fa-arrow-right');
             $('.navbar.navbarAtas.showed').removeClass('showed');
             $('.navbar.navbarKiri').addClass('notShowed');
+            $('.isi').addClass('notShowed');
             // $('.navbar.navbarKiri ul').addClass('notShowed');
             console.log('berhasil kiri');
         }
@@ -27,7 +29,11 @@ $(document).ready(function() {
     var scr1 = window.pageYOffset;
     window.onscroll = function() {
         var scr2 = window.pageYOffset;
-        if (scr1 > scr2) {
+        if (scr2 <= 400) {
+            $('.navbar.navbarAtas').css('top', '0px');
+            console.log('ganteng4');
+
+        } else if (scr1 > scr2) {
             $('.navbar.navbarAtas').css('top', '0px');
 
             $('.navbar.navbarAtas').on('mouseover', function() {
@@ -43,7 +49,7 @@ $(document).ready(function() {
 
                 } else {
                     $('.navbar.navbarAtas').css('top', '-100px');
-                    $('.navbar.navbarKiri').css('left', '-20%');
+                    // $('.navbar.navbarKiri').css('left', '-20%');
                     console.log('ganteng3');
                     clearInterval(inter);
 
