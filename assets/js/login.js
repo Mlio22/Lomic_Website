8b73d0f31deb6be463de017f5cb964094319b5ba
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2018-11-02 21:53:51 +0700

    git push .

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,20 @@ $(document).ready(function() {
 
     });
 
+    // JIKA SALAH SATU INPUT SEDANG FOKUS
+
+    $('input').on('focus', function() {
+        // $('label.active').removeClass('active');
+        $(this).siblings('label').addClass('active');
+    });
+
+    // KEBALIKAN DARI FUNGSI DIATAS JIKA FOCUSOUT
+    // KECUALI JIKA KOTAK TELAH TERISI
+
+    $('input').on('focusout', function() {
+        if ($(this).val() == '') {
+            $(this).siblings('label').removeClass('active');
+        }
+    });
+
 });
