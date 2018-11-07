07e0eeff665d8af956ae46887ce899901a768abd
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2018-11-07 07:27:24 +0700

    git push .

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,8 @@ $(document).ready(function() {
     $('input').on('focus', function() {
         // $('label.active').removeClass('active');
         $(this).siblings('label').addClass('active');
+        $(this).siblings('.garisBawah').addClass('active');
+
     });
 
     // KEBALIKAN DARI FUNGSI DIATAS JIKA FOCUSOUT
@@ -21,6 +23,18 @@ $(document).ready(function() {
     $('input').on('focusout', function() {
         if ($(this).val() == '') {
             $(this).siblings('label').removeClass('active');
+        }
+        $(this).siblings('.garisBawah.active').removeClass('active');
+
+    });
+
+    $('input').on('invalid', function() {
+        if ($(this).val() == '') {
+            $(this).css('border', '0px');
+            $(this).siblings('.garisBawah.error').removeClass('error');
+        } else {
+            $(this).css('border', '0px');
+            $(this).siblings('.garisBawah').addClass('error');
         }
     });
 
